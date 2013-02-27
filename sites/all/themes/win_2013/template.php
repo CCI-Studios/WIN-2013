78046283a921108fd2f888dc21f2f023336204a7
Jonathan Bennett <jbennett@ccistudios.com>
2013-02-27 12:00:07 -0500

    My Theme

---
+++ sites/all/themes/win_2013/template.php
@@ -0,0 +1,15 @@
+<?php
+
+// add menu classes to all menus
+function win_2013_preprocess_menu_link(&$vars) {
+ 	$classes = $vars['element']['#attributes']['class'];
+ 	$name = $vars['element']['#title'];
+ 	$name = 'menu'. str_replace(' ', '', ucfirst($name));
+
+ 	$classes[] = $name;
+ 	$vars['element']['#attributes']['class'] = $classes;
+}
+
+function win_2013_preprocess_views_view(&$vars) {
+
+}
