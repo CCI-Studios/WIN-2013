7ffbd006f7075e8533a4504fa287f3d61d746de3
Jonathan Bennett <jbennett@ccistudios.com>
2013-02-27 22:53:59 -0500

    Styled login block

--- sites/all/themes/win_2013/template.php
+++ sites/all/themes/win_2013/template.php
@@ -13,3 +13,21 @@ function win_2013_preprocess_menu_link(&$vars) {
 function win_2013_preprocess_views_view(&$vars) {
 
 }
+
+function win_2013_theme(&$existing, $type, $theme, $path) {
+	$hooks['user_login_block'] = array(
+		'template' => 'templates/user-login-block',
+		'render element' => 'form',
+	);
+
+	return $hooks;
+}
+
+function win_2013_preprocess_user_login_block(&$vars) {
+	$vars['form']['links'] = null; // kill links
+
+	$vars['name'] = render($vars['form']['name']);
+	$vars['pass'] = render($vars['form']['pass']);
+	$vars['submit'] = render($vars['form']['actions']['submit']);
+	$vars['rendered'] = drupal_render_children($vars['form']);
+}
