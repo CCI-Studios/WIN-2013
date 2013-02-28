7e1baba97b62145afa8591eaac4e4e04fda65902
Jonathan Bennett <jbennett@ccistudios.com>
2013-02-28 00:43:27 -0500

    more stuff

--- sites/all/themes/win_2013/template.php
+++ sites/all/themes/win_2013/template.php
@@ -23,6 +23,35 @@ function win_2013_theme(&$existing, $type, $theme, $path) {
 	return $hooks;
 }
 
+function win_2013_form_alter(&$form, &$form_state, $form_id) {
+	$containers = array('container', 'fieldset');
+	$fields = array('textfield', 'password');
+
+	// add placeholders everywhere
+	// foreach($form as $field => $values) {
+	// 	if (is_array($values) && isset($values['#type']) &&
+	// 	 in_array($values['#type'], $fields) && isset($values['#title'])) {
+	// 		$form[$field]['#attributes']['placeholder'] = $values['#title'];
+	// 		$form[$field]['#title'] = null;
+	// 	}
+	// }
+
+	// redirect after register
+	if ($form_id == 'user_register_form') {
+		$form_state['redirect'] = 'node/add/member';
+	}
+
+	if ($form_id == 'member_node_form') {
+		// var_dump("<pre>");
+		// var_dump($form['field_google_maps_link']['und'][0]['title']);
+		// var_dump("</pre>");
+
+		$form['field_google_maps_link']['und'][0]['title']['#type'] = 'hidden';
+		$form['field_google_maps_link']['und'][0]['title']['#default_value'] = 'Google Maps';
+	}
+}
+
+
 function win_2013_preprocess_user_login_block(&$vars) {
 	$vars['form']['links'] = null; // kill links
 
