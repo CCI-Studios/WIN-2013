8a218d797267828b9b23a32ec9f8aec6b004209f
Jonathan Bennett <jbennett@ccistudios.com>
2013-04-05 14:34:54 -0400

    Last minute changes

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -13,3 +13,17 @@ $aliases['dev'] = array(
 
 	//'ssh-options' => "-p 37241",
 );
+
+$aliases['live'] = array(
+	'uri' => 'winsarnia.com',
+	'root' => '/home/win/subdomains/live/public_html',
+	'remote-host' => 'winsarnia.com',
+	'remote-user' => 'win',
+
+	'path-aliases' => array(
+		'%dump-dir' => '/home/win/subdomains/live/_dumps',
+		'%files'	=> 'sites/default/files',
+	),
+
+	//'ssh-options' => "-p 37241",
+);
